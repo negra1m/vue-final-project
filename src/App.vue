4a97fb7679a4b28aac626b39efb4e25a78315dec
Vinicius Negrao de Menezes <vnsn_@outlook.com>
2019-12-17 00:14:48 -0300

    Feat: Creating Main Screen Layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from "./components/Main.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Main
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: rgb(51, 176, 201);
+  background: radial-gradient(
+    circle,
+    rgba(51, 176, 201, 1) 1%,
+    rgba(9, 52, 73, 1) 84%
+  );
+  height: 100vh;
+  width: 100vw;
+}
+
+body {
+  font-family: "Courier New", Courier, monospace;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="main">
+    <div class="content">
+      <div class="logo">
+        <img src="../assets/portal.gif" />
+        <button class="start">START</button>
+      </div>
+      <div class="text">
+        <h2>The Rick Morty`s API consumer</h2>
+        <h4>Developed in Vue</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.text {
+  color: #faff64;
+  padding: 15px 15px;
+  position: absolute;
+  margin-top: 160px;
+  text-align: center;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.start {
+  position: absolute;
+  background: none;
+  color: white;
+  border: none;
+  padding: 0;
+  font: inherit;
+  font-size: 3em;
+  font-weight: 600;
+  cursor: pointer;
+  outline: inherit;
+  filter: blur(2px);
+  -webkit-animation: rotate-center 6s ease-in-out infinite forwards;
+  animation: rotate-center 6s ease-in-out infinite forwards;
+}
+
+.start:hover {
+  filter: none;
+  animation: rotate-center 1s ease-in-out;
+  transition: all;
+}
+
+.content {
+  margin: 0px;
+  padding: 0px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+@-webkit-keyframes rotate-center {
+  0% {
+    -webkit-transform: rotate(0);
+    transform: rotate(0);
+  }
+
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes rotate-center {
+  0% {
+    -webkit-transform: rotate(0);
+    transform: rotate(0);
+  }
+
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+</style>
